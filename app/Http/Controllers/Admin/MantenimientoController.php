4b915d792707fe60842564cdf5f8bd08f2c7e51e
Jacky984-max <mjcordova2@utpl.edu.ec>
2025-01-30 23:53:45 -0500

    asignacion de permisos y ajustes en la UI del modulo de Registro

--- app/Http/Controllers/Admin/MantenimientoController.php
+++ app/Http/Controllers/Admin/MantenimientoController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreMantenimientoRequest;
 use App\Models\FlotaVehicular;
 use App\Models\Mantenimiento;
+use App\Models\Personal_policial;
+use App\Models\Vehiculo;
 use Illuminate\Http\Request;
 
 class MantenimientoController extends Controller
@@ -16,9 +18,12 @@ class MantenimientoController extends Controller
     public function index()
     {
         //
-        $mantenimiento = Mantenimiento::select('mantenimientos.id', 'tipo_mantenimiento', 'descripcion', 'solicitante', 'fecha_hora', 'observacion', 'flotavehicular_id', 'placa', 'estado')
+        $mantenimiento = Mantenimiento::all();
 
-        ->join('flota_vehiculars', 'flota_vehiculars.id', '=', 'mantenimientos.flotavehicular_id')
+        $mantenimiento = Mantenimiento::select('mantenimientos.id', 'tipo_mantenimiento', 'descripcion', 'fecha_hora', 'observacion', 'flotavehicular_id', 'placa', 'estado', 'solicitante_id', 'nombres')
+
+        ->join('vehiculos', 'vehiculos.id', '=', 'mantenimientos.flotavehicular_id')
+        ->join('personal_policials', 'personal_policials.id', '=', 'mantenimientos.solicitante_id')
 
         ->get();
 
@@ -31,8 +36,10 @@ class MantenimientoController extends Controller
     public function create()
     {
         //
-        $solicitud = FlotaVehicular::all();
-        return view('admin.mantenimiento_vehicular.create', compact('solicitud'));
+        $solicitud = Vehiculo::all();
+        $personal = Personal_policial::all();
+
+        return view('admin.mantenimiento_vehicular.create', compact('solicitud', 'personal'));
     }
 
     /**
@@ -45,13 +52,12 @@ class MantenimientoController extends Controller
             
             'tipo_mantenimiento' => $request->tipo_mantenimiento,
             'descripcion' => $request->descripcion,
-            'solicitante' => $request->solicitante,
             'fecha_hora' => $request->fecha_hora,
             'kilometraje' => $request->kilometraje,
             'observacion' => $request->observacion,
             'flotavehicular_id' => $request->flotavehicular_id,
-            //'estado' => $request->estado,
-
+            'solicitante_id' => $request->solicitante_id,
+            
         ]);
 
         $mante_vehicular->saveOrFail();
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,107 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<!DOCTYPE html>
+<html lang="en">
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+<!-- auth-register.html  21 Nov 2019 04:05:01 GMT -->
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Registro</title>
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="{{ asset('new/assets/css/app.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('new/assets/bundles/jquery-selectric/selectric.css') }}">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ asset('new/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('new/assets/css/components.css') }} ">
+    <!-- Custom style CSS -->
+    <link rel="stylesheet" href="{{ asset('new/assets/css/custom.css') }} ">
+    <link rel='shortcut icon' type='image/x-icon' href='{{ asset('new/assets/img/favicon.ico') }}' />
+</head>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+<body>
+    <div class="loader"></div>
+    <div id="app">
+        <section class="section">
+            <div class="container mt-5">
+                <div class="row">
+                    <div
+                        class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h4>Create una Cuenta</h4>
+                            </div>
+                            <div class="card-body">
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                                <form method="POST" action="{{ route('register') }}" class="js-validate needs-validation" novalidate>
+                                    @csrf
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                                    <div class="row">
+                                        <div class="form-group col-6">
+                                            <label for="frist_name">Nombres</label>
+                                            <input id="name" type="text" class="form-control" name="name"
+                                                placeholder="Digita tus nombres" :value="old('name')" required
+                                                autofocus>
+                                        </div>
+                                        <!--<div class="form-group col-6">
+                      <label for="last_name">Last Name</label>
+                      <input id="last_name" type="text" class="form-control" name="last_name">
+                    </div>-->
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="email">Correo</label>
+                                        <input id="email" type="email" placeholder="Digita un correo"
+                                            class="form-control" name="email" :value="old('email')" required>
+                                    </div>
+                                    <div class="row">
+                                        <div class="form-group col-6">
+                                            <label for="password" class="d-block">Contraseña</label>
+                                            <input id="password" type="password" class="form-control pwstrength"
+                                                placeholder="Digita una contraseña" data-indicator="pwindicator"
+                                                name="password" required>
+                                            <!--<div id="pwindicator" class="pwindicator">
+                        <div class="bar"></div>
+                        <div class="label"></div>
+                      </div>-->
+                                        </div>
+                                        <div class="form-group col-6">
+                                            <label for="password2" class="d-block">Confirmar Contraseña</label>
+                                            <input id="password_confirmation" type="password" class="form-control"
+                                                placeholder="Confirmar Contraseña" name="password_confirmation" required>
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+                                        </div>
+                                    </div>
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                            REGISTRATE
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="mb-4 text-muted text-center">
+                                ¿Ya estás registrado? <a href="{{ route('login') }}">Iniciar Sesión</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @include('components.footer.footer')
+            </div>
+        </section>
+    </div>
+    <!-- General JS Scripts -->
+    <script src="{{ asset('new/assets/js/app.min.js') }}"></script>
+    <!-- JS Libraies -->
+    <script src="{{ asset('new/assets/bundles/jquery-pwstrength/jquery.pwstrength.min.js') }}"></script>
+    <script src="{{ asset('new/assets/bundles/jquery-selectric/jquery.selectric.min.js') }}"></script>
+    <!-- Page Specific JS File -->
+    <script src="{{ asset('new/assets/js/page/auth-register.js') }}"></script>
+    <!-- Template JS File -->
+    <script src="{{ asset('new/assets/js/scripts.js') }}"></script>
+    <!-- Custom JS File -->
+    <script src="{{ asset('new/assets/js/custom.js') }}"></script>
+</body>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+
+</html>
